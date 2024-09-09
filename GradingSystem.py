f33699672a95aa45acd3884da70edcccd6f13618
Umesh Pradip Sanap <128315120+umeshsanap@users.noreply.github.com>
2024-09-09 04:53:16 -0700

    GradingSystem.py

--- GradingSystem.py
+++ GradingSystem.py
@@ -1,5 +1,5 @@
 marks = int(input("Enter the marks to check the grade :-"))
-if marks >= 90:
+if marks >= 90 and marks <= 100:
     print("Grade : A")
 elif marks == 80 and marks < 90:
     print("Grade : B")
@@ -9,5 +9,7 @@ elif marks == 60 and marks < 70:
     print("Grade : D")
 elif 35 <= marks <= 60:
     print("Grade : E")
+elif 0 <= marks < 35:
+    print("You are fail!")
 else:
-    print("You are fail")
+    print("You entered wrong data!")
