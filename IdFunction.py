b2aaea3c041f60d062f7c82cdaf45e2e4ae973ec
Umesh Pradip Sanap <128315120+umeshsanap@users.noreply.github.com>
2024-09-15 22:05:13 -0700

    Add files via upload
    **Id function**
    It print id of the variable.

---
+++ IdFunction.py
@@ -0,0 +1,8 @@
+intNumber = int(input("Enter integer number :-"))
+print(id(intNumber))
+
+floatNumber = float(input("Enter float number :-"))
+print(id(floatNumber))
+
+strName = input("Enter your name :-")
+print(id(strName))
