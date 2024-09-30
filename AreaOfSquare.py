c246ce91120dce2b1774663e6dc6f5847500b011
Umesh Pradip Sanap <128315120+umeshsanap@users.noreply.github.com>
2024-09-30 04:27:59 -0700

    Area Of Square
    Take input from the user and print area of square

---
+++ AreaOfSquare.py
@@ -0,0 +1,4 @@
+# Write a program to take input from user and print the area of square
+intSide = int(input("Enter the side of square :-"))
+area = intSide * intSide
+print("Area Of Square :-",area)
