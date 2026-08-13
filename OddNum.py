ac98732643ad829c30d435a28a0ed0ddda844af2
umeshsanap <umeshsanap2809@gmail.com>
2026-08-13 09:08:22 +0530

    Find odd number from 1 to 100 using for loop and while loop

---
+++ OddNum.py
@@ -0,0 +1,12 @@
+'''
+Print all odd numbers between 1 and 100
+'''
+# for i in range(1, 101):
+#     if i % 2 != 0:
+#         print(i, end = " ")
+
+i = 1
+while(i<=100):
+    if i%2!=0:
+        print(i, end = " ")
+    i = i + 1
