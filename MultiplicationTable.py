234c5500837fcbaa787d9e5807b5dcda93ce45a1
umeshsanap <umeshsanap2809@gmail.com>
2026-08-13 09:20:41 +0530

    Print the multiplication table of number

---
+++ MultiplicationTable.py
@@ -0,0 +1,13 @@
+'''
+5. Print the multiplication table of a given number from n × 1 to n × 10.
+'''
+n = int(input("\nEnter the number for printing table :- "))
+i = 1
+print("\n",55*"=")
+print(20*" ",f"Table for {n}")
+print(55*"=")
+while (i <=10):
+    cal = n * i
+    print(25*" ",cal)
+    i = i+1
+print(55*"=","\n")
