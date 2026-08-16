db6faa757d6cb338c8dc72e54ff2e732239172cd
umeshsanap <umeshsanap2809@gmail.com>
2026-08-16 08:55:12 +0530

    Sum of odd number from n number entered by the user

--- SumOfOddNumFromOne.py
+++ SumOfOddNumFromOne.py
@@ -1,11 +1,13 @@
-'''
-Find the sum of all odd number from 1 to n
-'''
-n = int(input("Enter the number :- "))
-sum = 0
-i = 1
-while(i<=n):
-    if i % 2 != 0:
-        sum = sum + i
-    i += 1
-print(f"Sum of all odd number from 1 to {n} is {sum}")
+num = int(input("Enter the number from where you want odd number :- "))
+class OddNum:
+    def findOddNum(n):
+        i = 0
+        sum = 0
+        while(i<=n):
+            if i%2!=0:
+                sum = sum+i
+            i += 1
+        print(f"Sum of all odd number from {n} is {sum}")
+
+obj = OddNum
+obj.findOddNum(num)
